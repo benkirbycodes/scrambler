edaeeaa1851cdd36ad966efa35d2690d984f54da
benkirbycodes <benkirbycodes@gmail.com>
2020-02-26 11:35:39 -0700

    Auth works, Can login to front end

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "benkirbycodes.auth0.com";
-export const audience = "https://benkirbycodes-api.com";
-export const clientId = "elVnw8LFkDqF0ciWNFusNULTaKszAEwO";
+export const audience = "https://scrambler-api.com";
+export const clientId = "EXE5S23jsEEQm4toca8Uww4c5q3l4xHY";
